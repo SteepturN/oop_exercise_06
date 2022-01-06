f3f39d1dbce6d32dcc4b855d8bf60db2e11e9d1a
admin <admin@gg>
2022-01-06 19:42:51 +0300

    working version

--- header/Allocator.hpp
+++ header/Allocator.hpp
@@ -4,6 +4,11 @@
 #include <iostream>
 #include <stack>
 #include <memory>
+struct Block {
+	size_t size;
+	void* memory;
+	Block(size_t, void*);
+};
 template <typename T, int Size>
 struct Allocator {
 	using value_type = T;
@@ -22,8 +27,9 @@ struct Allocator {
 	inline constexpr std::size_t max_size() const noexcept {return 1;}
 //stupid task == stupid allocator
 	std::shared_ptr<char[]> storage;
-	std::shared_ptr<std::stack<T*>> free_elements;
+	std::shared_ptr<std::stack<Block>> free_elements;
 };
+
 template<class T1, int Sz1, class T2, int Sz2>
 bool operator==(const Allocator<T1, Sz1>& lhs, const Allocator<T2, Sz2>& rhs);
 #endif // ALLOCATOR_H_
--- source/Allocator.cpp
+++ source/Allocator.cpp
@@ -2,45 +2,54 @@
 #include <type_traits>
 #include <typeinfo>
 #include <string>
+Block::Block(size_t size, void* memory)
+	: size(size), memory(memory) {}
 template <typename T, int Size>
 Allocator<T,Size>::~Allocator(){
-	std::cout <<"Allocator destroyed with size = " << Size << " and type = " << typeid(T).name() <<'\n';
+//	std::cout <<"Allocator destroyed with size = " << Size << " and type = " << sizeof(T) <<'\n';
 }
 
 template <typename T, int Size>
 Allocator<T, Size>::Allocator() :
-	storage(new char[sizeof(T) * Size]), free_elements(new std::stack<T*>) {
-	std::cout <<"Allocator() used with Size = " << Size << " and type = " << typeid(T).name() <<'\n';
+	storage(new char[Size*1024]), free_elements(new std::stack<Block>) {
+//	std::cout <<"Allocator() used with Size = " << Size << " and type = " << sizeof(T) <<'\n';
 	if(storage == nullptr) throw std::runtime_error("can't allocate storage");
 	if(free_elements == nullptr) throw std::bad_alloc();
-	for(int i = 0; i < Size; i++) {
-		free_elements->push(reinterpret_cast<T*>(
-			                    static_cast<char*>(storage.get()) + i * sizeof(T)));
-	}
+	free_elements->push(Block(Size*1024, static_cast<void*>(storage.get())));
 }
-template<class T, int Size> template<class U, int Size2> Allocator<T, Size>::Allocator(const Allocator<U, Size2>&) :
-	storage(new char[sizeof(T) * Size]), free_elements(new std::stack<T*>) {
-	std::cout << "Allocator<T,Size>(Allocator<U,Size2>) used with Size = " << Size << " and size of type = " << typeid(T).name() <<'\n';
-	std::cout << "and Size2 = " <<Size2 <<" and size of type U = " << typeid(U).name() <<'\n';
+template<class T, int Size> template<class U, int Size2> Allocator<T, Size>::Allocator(const Allocator<U, Size2>& all) :
+	storage(all.storage), free_elements(all.free_elements) {
+//	std::cout << "Allocator<T,Size>(Allocator<U,Size2>) used with Size = " << Size << " and size of type = " << sizeof(T) <<'\n';
+//	std::cout << "and Size2 = " <<Size2 <<" and size of type U = " << sizeof(U) <<'\n';
 	if(storage == nullptr) throw std::runtime_error("can't allocate storage");
 	if(free_elements == nullptr) throw std::bad_alloc();
-	for(int i = 0; i < Size; i++) {
-		free_elements->push(reinterpret_cast<T*>(
-			                    static_cast<char*>(storage.get()) + i * sizeof(T)));
-	}
 }
 template <typename T, int Size>
 T* Allocator<T, Size>::allocate(int n) {
-	std::cout << "allocate(" << n << ") used with Size = " << Size << " and size of type = " <<sizeof(T) <<'\n';
+//	std::cout << "allocate(" << n << ") used with Size = " << Size << " and size of type = " <<sizeof(T) <<'\n';
 	if(n != 1 || free_elements->empty()) return nullptr;
-	T* ret = free_elements->top();
-	free_elements->pop();
-	return ret;
+	char* allocated_memory = nullptr;
+	bool found = false;
+	while(!found) {
+		Block block = free_elements->top();
+		free_elements->pop();
+		if(block.size > sizeof(T)) {
+			allocated_memory = static_cast<char*>(block.memory);
+			free_elements->push(Block(block.size-sizeof(T), static_cast<char*>(block.memory+sizeof(T))));
+			found = true;
+		} else if (block.size == sizeof(T)) {
+			allocated_memory = static_cast<char*>(block.memory);
+			found = true;
+		} else if (free_elements->size() == 0) {
+			throw std::bad_alloc();
+		}
+	}
+	return reinterpret_cast<T*>(allocated_memory);
 }
 template <typename T, int Size>
-void Allocator<T, Size>::deallocate(T* p, int) {
+void Allocator<T, Size>::deallocate(T* p, int n) {
 //	std::cout << "deallocate(" << p << ") used with Size = " << Size << " and size of type = " <<sizeof(T) <<'\n';
-	free_elements->push(p);
+	free_elements->push(Block(n, static_cast<void*>(p)));
 }
 template<class T1, int Sz1, class T2, int Sz2>
 bool operator==(const Allocator<T1, Sz1>& lhs, const Allocator<T2, Sz2>& rhs) {
